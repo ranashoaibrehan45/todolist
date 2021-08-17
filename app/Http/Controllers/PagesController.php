c997e77212045a1d606b4486ec2b45ecb613db9d
ranashoaibrehan45 <ranashoaibrehan@gmail.com>
2021-08-17 15:57:11 +0500

    db dump changes

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Spatie\DbDumper\Databases\MySql;
+use Illuminate\Support\Facades\Artisan;
 
 class PagesController extends Controller
 {
@@ -14,10 +14,10 @@ class PagesController extends Controller
 
     public function databaseBackup()
     {
-        MySql::create()
-            ->setDbName(config('database.connections.mysql.database'))
-            ->setUserName(config('database.connections.mysql.username'))
-            ->setPassword(config('database.connections.mysql.password'))
-            ->dumpToFile('dump.sql');
+        //echo date('Y-m-d-H-i-s');
+        //echo exec('php artisan backup:run');
+        Artisan::call('backup:run');
+        $path = storage_path('app/Todo-List/' . date('Y-m-d-H-i-s') . '.zip');
+        return response()->download($path);
     }
 }
